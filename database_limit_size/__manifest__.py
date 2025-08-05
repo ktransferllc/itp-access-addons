211a686b09850f7f97cc3f1eccd329526554b7b3
anderson <andersonvidal94@gmail.com>
2025-08-05 10:21:47 -0500

    [ADD] modules

--- database_limit_size/__manifest__.py
+++ database_limit_size/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": """This module allows blocking backend access when database limit is exceeded""",
     "category": "Extra Tools",
     "images": [],
-    "version": "15.0.1.0.0",
+    "version": "18.0.1.0.0",
     "application": False,
     "author": "IT-Projects LLC, Eugene Molotov",
     "support": "it@it-projects.info",
